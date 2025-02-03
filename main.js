75be9000e0d0dae004db88c85764693f4d87af2f
RafaelKrewer <cesar.krewer@gmail.com>
2025-02-03 16:30:01 -0300

    botando javascript img

---
+++ main.js
@@ -0,0 +1,12 @@
+var imgf = new Image();
+imgf.src = "000-15M_f.webp";
+var imgb = new Image();
+imgb.src = "000-15M_b.webp";
+
+function swap() {
+    imagem1.src = imgf.src;
+}
+
+function back() {
+    imagem1.src = imgb.src;
+}
