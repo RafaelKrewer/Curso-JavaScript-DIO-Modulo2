12a027d3a334a38f6d2d11327fa2eee5f5385055
RafaelKrewer <rafaelkrewer5@gmail.com>
2025-02-18 16:08:16 -0300

    Add initial files for guitar sales website, including HTML, CSS, images, and JavaScript functionality

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center"> Site </h1>
+
+# Índice 
+
+ <p> 
+ 
+ <a>Tecnologias utilizadas: JavaScript, HTML, CSS</a>
+ 
+ <a>Desenvolvedores do Projeto: Rafael Krewer</a>
+ 
+ <p>
+> :construction: Projeto em construção :construction:
---
+++ main.js
@@ -0,0 +1,16 @@
+var imgf = new Image();
+imgf.src = "000-15M_f.novo.png";
+
+var imgb = new Image();
+imgb.src = "000-15M_b.webp";
+
+
+var imagem = document.getElementsByName("imagem1")[0];
+
+function swap() {
+    imagem.src = imgb.src;
+}
+
+function back() {
+    imagem.src = imgf.src;
+}
